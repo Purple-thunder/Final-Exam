38058a34bfa51c591c23a3e427d8a77e58b1afaa
Purple-thunder <86098072+Purple-thunder@users.noreply.github.com>
2023-08-14 14:05:58 -0400

    first commit

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() 
+{
+
+  // This function will load global footer and header
+  $(function()
+  {
+    $('#footer').load('footer.html');
+    $('#header').load('header.html');
+  });
+});
